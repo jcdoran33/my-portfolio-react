2a81f166d93f2a0160bd99a1d0e8965bcefb3b46
Jack Doran <jcdoran33@gmail.com>
2022-07-24 16:38:30 -0400

    code cleanup, removal of unecessary code and comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg'; // og/default import
 import './App.css'; //og/default import
 
 //import React from react
@@ -10,29 +9,4 @@ import PortfolioContainer from "./components/PortfolioContainer";
 const App = () => <PortfolioContainer />;
 
 export default App;
-
-//==============================
-// og content below***
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//end of og content***
-
 
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -71,7 +71,7 @@ const projectObj = [
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About'); //default to about me page
 
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  // render correct component, based on currentPage value
   const renderPage = () => {
     if (currentPage === 'About') {
       return <About />;
@@ -89,11 +89,13 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
+      
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
+      
       {renderPage()}
+
       <Footer />
+      
     </div>
   );
 }
