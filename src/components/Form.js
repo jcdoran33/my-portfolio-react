4fb201d113df1e499e90f87edaedb50416149de6
Jack Doran <jcdoran33@gmail.com>
2022-08-04 14:19:05 -0400

    added sucess message on form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,11 +14,16 @@ export default function Form() {
             console.log(result.text);
         }, (error) => {
             console.log(error.text);
-        });
+        })
+        .then(()=> document.querySelector(".alert").style.display='flex')
+        .then(()=> document.querySelector("#contact-form").reset());
     };
     // end new content for email js
     return (
         <div>
+            <div className="alert alert-success" role="alert">
+                Success! Email with form content has been sent. I will get back to you ASAP!
+            </div>
             <h3>Contact Form</h3>
             <form id="contact-form" ref={form} onSubmit={sendEmail}>
                 <input type="hidden" name="contact_number"></input>
