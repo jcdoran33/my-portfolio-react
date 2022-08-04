8b6c4d35a939d13da8b1a399bb81d298d77cafb7
Jack Doran <jcdoran33@gmail.com>
2022-08-04 12:11:11 -0400

    made footer links open in no tab, added noreferrer tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
                     <span className="text-muted col-sm blink-container">Let's connect! <span className="blink">{'>>>>>>>>>>>>>'}</span></span>
                     <div className="col-sm">
                         {/* <!-- Logo links for Github linked in here --> */}
-                        <a href="https://www.linkedin.com/in/j-doran/"><img className="logos m-3" alt="LinkedIn Logo" src="./LI-In-Bug.png" /></a>
-                        <a href="https://github.com/jcdoran33"><img className="logos m-3" alt="Github Logo" src="./GitHub-Mark-64px.png" /></a>
-                        <a href="https://stackoverflow.com/users/19583492/jack"><img className="logos m-3" alt="Stack Overflow Logo" src="./Stack_Overflow-Icon-Logo.wine.png" /></a>
+                        <a href="https://www.linkedin.com/in/j-doran/" target="_blank" rel="noreferrer"><img className="logos m-3" alt="LinkedIn Logo" src="./LI-In-Bug.png" /></a>
+                        <a href="https://github.com/jcdoran33" target="_blank" rel="noreferrer"><img className="logos m-3" alt="Github Logo" src="./GitHub-Mark-64px.png" /></a>
+                        <a href="https://stackoverflow.com/users/19583492/jack" target="_blank" rel="noreferrer"><img className="logos m-3" alt="Stack Overflow Logo" src="./Stack_Overflow-Icon-Logo.wine.png" /></a>
                     </div>
                 </div>
             </div>
