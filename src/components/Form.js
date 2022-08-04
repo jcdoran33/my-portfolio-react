2d64b838719f68caf6d1ce4542a9af34d40b8d3a
Jack Doran <jcdoran33@gmail.com>
2022-08-04 13:03:14 -0400

    implemented email js for contact form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
+//import emailjs
+import emailjs from '@emailjs/browser';
 
 export default function Form() {
+    // new content for email js
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('contact_service', 'contact_form', form.current, 'iknfIbiyFVzfpzwOX')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+    // end new content for email js
     return (
         <div>
             <h3>Contact Form</h3>
-            <form id="contact-form">
+            <form id="contact-form" ref={form} onSubmit={sendEmail}>
                 <input type="hidden" name="contact_number"></input>
                 <input name="user_name" type="text" placeholder="Name" required={true}></input> <br /><br />
                 <input name="user_email" type="email" placeholder="Email" required={true}></input> <br /><br />
