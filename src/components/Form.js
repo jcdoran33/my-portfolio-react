a57ee24d62d39c02d7ca806bc1a447988997eba6
Jack Doran <jcdoran33@gmail.com>
2022-08-04 13:07:43 -0400

    minor edits - placeholder adj on form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -22,8 +22,8 @@ export default function Form() {
             <h3>Contact Form</h3>
             <form id="contact-form" ref={form} onSubmit={sendEmail}>
                 <input type="hidden" name="contact_number"></input>
-                <input name="user_name" type="text" placeholder="Name" required={true}></input> <br /><br />
-                <input name="user_email" type="email" placeholder="Email" required={true}></input> <br /><br />
+                <input name="user_name" type="text" placeholder="Your Name" required={true}></input> <br /><br />
+                <input name="user_email" type="email" placeholder="Your Email" required={true}></input> <br /><br />
                 <textarea name="message" cols="40" rows="7" required={true} placeholder="Enter message here...">
 
                 </textarea>
