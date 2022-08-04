fbe4ecda6e81c81cb0122c34c0021ea46cffffc3
Jack Doran <jcdoran33@gmail.com>
2022-08-04 12:52:44 -0400

    minor edits to Form.js to prep for email js implementation

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,11 @@ export default function Form() {
     return (
         <div>
             <h3>Contact Form</h3>
-            <form>
-                <input name="name" type="text" placeholder="Name" required={true}></input> <br /><br />
-                <input name="email" type="email" placeholder="Email" required={true}></input> <br /><br />
-                <textarea cols="40" rows="7" required={true} placeholder="Enter message here...">
+            <form id="contact-form">
+                <input type="hidden" name="contact_number"></input>
+                <input name="user_name" type="text" placeholder="Name" required={true}></input> <br /><br />
+                <input name="user_email" type="email" placeholder="Email" required={true}></input> <br /><br />
+                <textarea name="message" cols="40" rows="7" required={true} placeholder="Enter message here...">
 
                 </textarea>
                 <br />
