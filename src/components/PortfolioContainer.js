2696b905c49b089cb3dac82ce450bb66ed94935a
Jack Doran <jcdoran33@gmail.com>
2022-08-15 12:15:41 -0400

    added new project placeKaya to portfolio page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -15,11 +15,20 @@ import NoteTakerScreenshot from '../../src/images/note-taker-screenshot.png';
 import ReadMeGeneratorScreenshot from '../../src/images/readme-generator-screenshot.png';
 import WeatherDashboardScreenshot from '../../src/images/weather-dash-screenshot.png';
 import ExcelsiorScreenshot from '../../src/images/excelsior-screenshot.png';
+import PlaceKayaScreenshot from '../../src/images/placekaya-screenshot-small.png';
 
 //  test - laying out object with project data
 const projectObj = [
   {
     id: 1,
+    projectName: "placeKaya.com",
+    projectImageLink: PlaceKayaScreenshot,
+    projectDescription: "A custom-built placeholder image service featuring images of my dog, Kaya.",
+    projectDeployedLink: "https://www.placekaya.com",
+    projectGithubRepo: "https://github.com/jcdoran33/PlaceKaya"
+  },
+  {
+    id: 2,
     projectName: "Adventure Adviser",
     projectImageLink: AdvAdvScreenshot,
     projectDescription: "Allows a user to search and buy tickets for events in a given geographical area. Utilizes Google Maps, OpenWeather, and Ticketmaster APIs",
@@ -27,7 +36,7 @@ const projectObj = [
     projectGithubRepo: "https://github.com/jcdoran33/Adventure-Adviser"
   },
   {
-    id: 2,
+    id: 3,
     projectName: "Excelsior Emporium",
     projectImageLink: ExcelsiorScreenshot,
     projectDescription: "A mock ecommerce site for the Marvel universe. MERN-stack application using Apollo/GraphQL, bcrypt and json webtoken packages, and Stripe payment API",
@@ -35,7 +44,7 @@ const projectObj = [
     projectGithubRepo: "https://github.com/jcdoran33/Excelsior-Emporium"
   },
   {
-    id: 3,
+    id: 4,
     projectName: "Ecommerce Back End",
     projectImageLink: EcommerceScreenshot,
     projectDescription: "A simplified example of an ecommerce back end using Express with MySql/Sequelize. User can create, read, update, and delete products, product categories, and product tags.",
@@ -43,7 +52,7 @@ const projectObj = [
     projectGithubRepo: "https://github.com/jcdoran33/Ecommerce-Back-End"
   },
   {
-    id: 4,
+    id: 5,
     projectName: "Employee Tracker",
     projectImageLink: EmpTrackerScreenshot,
     projectDescription: "Back end CLI application that allows a user to create, read update, and delete from a database of employee information. Uses Node, Express, MySql, and the Inquirer npm package.",
@@ -51,7 +60,7 @@ const projectObj = [
     projectGithubRepo: "https://github.com/jcdoran33/Employee-Tracker"
   },
   {
-    id: 5,
+    id: 6,
     projectName: "Note Taker",
     projectImageLink: NoteTakerScreenshot,
     projectDescription: "A simple note taking application that will allow a user to create text notes, save them, and edit them. The note data persists, so if a user navigates away from the page and returns later, the notes from the previous session will be saved.",
@@ -59,7 +68,7 @@ const projectObj = [
     projectGithubRepo: "https://github.com/jcdoran33/Note-Taker"
   },
   {
-    id: 6,
+    id: 7,
     projectName: "ReadMe Generator",
     projectImageLink: ReadMeGeneratorScreenshot,
     projectDescription: "This is a CLI application that will automatically generate a ReadMe file for the end user, based on the user's supplied criteria. The application uses the Inquirer package to prompt the user for information about their project, then generates a ReadMe.MD file.",
@@ -67,7 +76,7 @@ const projectObj = [
     projectGithubRepo: "https://github.com/jcdoran33/ReadMe-Generator"
   },
   {
-    id: 7,
+    id: 8,
     projectName: "Weather Dashboard",
     projectImageLink: WeatherDashboardScreenshot,
     projectDescription: "This is a front end application that displays current and 5-day weather forecast for a given geographical area based on the user's input. It utilizes the OpenWeather API for current weather data, and the Google Maps API for geocoding.",
